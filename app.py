9f4533e0847d3cab1cd65513eb5bbb69c4e994a6
deokhyun-dv <dev.deokhyun@gmail.com>
2021-09-15 21:24:30 +0900

    로그인 업데이트

--- app.py
+++ app.py
@@ -1,54 +1,97 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
-import requests
-
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
 
 client = MongoClient('3.34.5.163', 27017, username="test", password="test")
-db = client.dbsparta_plus_week2
-
+db = client.dbsparta_plus_week4
 
 @app.route('/')
-def main():
-    # DB에서 저장된 단어 찾아서 HTML에 나타내기
-    return render_template("home.html")
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info, msg="로그인 완료")
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/user/<username>')
+def user(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('user.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
 
 
-# @app.route('/detail/<keyword>')
-# def detail(keyword):
-#     status_receive = request.args.get("status_give", "new")
-#     # url로 받는 parameter니까 args.get
-#     r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token d48249c2ea738e8467a58758336670a1410c9c41"})
-#     print(r)
-#     if r.status_code != 200:
-#         return redirect(url_for("main", msg="단어가 이상해요"))
-#     result = r.json()
-#     print(result)
-#     return render_template("detail.html", word=keyword, result=result, status=status_receive)
-#
-#
-# @app.route('/api/save_word', methods=['POST'])
-# def save_word():
-#     word_receive = request.form['word_give']
-#     definition_receive = request.form['definition_give']
-#     print(word_receive, definition_receive)
-#     # post 요청으로 받는 변수들이니까 request.form
-#     doc = {
-#         'word': word_receive,
-#         'definition': definition_receive
-#     }
-#     db.words.insert_one(doc)
-#     # 단어 저장하기
-#     return jsonify({'result': 'success', 'msg': f'단어 "{word_receive}"저장'})
-#
-#
-# @app.route('/api/delete_word', methods=['POST'])
-# def delete_word():
-#     word_receive = request.form['word_give']
-#     db.words.delete_one({'word': word_receive})
-#     # 단어 삭제하기
-#     return jsonify({'result': 'success', 'msg': f'단어 {word_receive}삭제'})
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    print(result)
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    # 회원가입
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        'username': username_receive,
+        'password': password_hash,
+        'profile_name': username_receive,
+        'profile_pic': "",
+        "profile_pic_real": "profile_pics/profile_placeholder.png",
+        'profile_info': ""
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({'username': username_receive}))
+    return jsonify({'result': 'success', 'exists': exists} )
+
 
 
 if __name__ == '__main__':
