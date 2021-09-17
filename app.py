a1b7da5f70ef0471aa31493731c920946a4e185b
deokhyun-dv <dev.deokhyun@gmail.com>
2021-09-17 11:20:33 +0900

    홈, 레시피홈 레이아웃 수정, 검색로직 추가, 레시피홈 로그인 적용

--- app.py
+++ app.py
@@ -91,6 +91,15 @@ def check_dup():
 # 검색창
 @app.route('/home')
 def receipe():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('home.html', user_info=user_info )
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
     return render_template('home.html')
 
 
